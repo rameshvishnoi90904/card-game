f7975735b3fb88defc7959f63e7873ab5cce6a63
Ramesh Vishnoi <rameshvishnoi90904@gmail.com>
2022-08-15 18:15:19 +0530

    chore: first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+    Text,
+    View,
+} from 'react-native';
+const App = () => {
+    return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text>Hello World</Text>
+        </View>
+    )
+}
+
+export default App;
