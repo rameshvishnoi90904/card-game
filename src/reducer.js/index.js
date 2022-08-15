ae395927177d800343a0f7d1be28610ea15c01d2
Ramesh Vishnoi <rameshvishnoi90904@gmail.com>
2022-08-15 18:33:53 +0530

    fix: formatting

--- src/reducer.js/index.js
+++ src/reducer.js/index.js
@@ -1,14 +1,14 @@
 function reducer(state, action) {
-    switch (action.type) {
-case 'increment':
-return {count: state.count + 1};
-case 'decrement':
-return {count: state.count - 1};
-case 'reset':
-return {count: 0}
-default:
-throw new Error();
-}
+  switch (action.type) {
+    case 'increment':
+      return {count: state.count + 1};
+    case 'decrement':
+      return {count: state.count - 1};
+    case 'reset':
+      return {count: 0};
+    default:
+      throw new Error();
+  }
 }
 
 export default reducer;
